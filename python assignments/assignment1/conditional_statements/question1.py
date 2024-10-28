7767c555f252668b15004b85a8814b2dc06deeff
Muhammad-Aman171 <ag3734359@gmail.com>
2024-10-28 12:00:10 +0500

    question2

---
+++ python assignments/assignment1/conditional_statements/question1.py
@@ -0,0 +1,10 @@
+
+
+userInput = int(input("enter your number :"))
+
+if userInput > 0 :
+    print( "this number", userInput , "is positive")
+elif userInput == 0 :
+    print( "this number", userInput , "is equal to zero")
+else:
+    print( "this number", userInput , "is nagetive")
---
+++ python assignments/assignment1/conditional_statements/question3.py
@@ -0,0 +1,10 @@
+
+
+userInput = int(input("enter your age : "))
+
+if userInput >= 50 :
+    print( "you are a senior citizen")
+elif userInput >= 18 and userInput <=49 :
+    print( "you are a Adult")
+else:
+    print( "your look like a minor age")
