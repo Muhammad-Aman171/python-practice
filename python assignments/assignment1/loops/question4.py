31ae6368d9c5c3ae78afd3bb57272350964c9afb
Muhammad-Aman171 <ag3734359@gmail.com>
2024-10-30 15:50:45 +0500

    question4

--- python assignments/assignment1/loops/question4.py
+++ python assignments/assignment1/loops/question4.py
@@ -2,12 +2,8 @@
 # 4. Print the multiplication table of a given number.
 
 count = int(input("enter your number : ")) 
-range = int(input("enter your number : ")) 
+# range = int(input("enter your range : ")) 
 
-# for i in range(int(input("enter your range : ")) ):
-    
-#     total =  count * i 
-#     print (count , "x" , i , "=" , total)
-
-
-# while 
+for i in range(1 , int(input("enter your range : ")) + 1 ):
+    total =  count * i 
+    print (count , "x" , i , "=" , total)
