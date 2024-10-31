3caa3ac0d7dcba61d29ca78e8de2da2689b50f6f
Muhammad-Aman171 <ag3734359@gmail.com>
2024-10-31 19:02:40 +0500

    question17

---
+++ python assignments/assignment1/loops/question17.py
@@ -0,0 +1,15 @@
+
+# 17. Write a program that continues to ask for a number until the correct number is guessed.
+
+# userInput = int(input('enter your number : '))
+
+num = 3
+
+while True : 
+    guess = int(input('enter your number : '))
+
+    if num == guess :
+        print('congratulation! you guessed the number')
+        break
+    else:
+        print('your number is wrong')
