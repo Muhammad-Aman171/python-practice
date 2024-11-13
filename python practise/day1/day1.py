1d660cc1f966f03cdf48a9ae50c9e843534d09b5
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-14 00:18:14 +0500

    working

---
+++ python practise/day1/day1.py
@@ -0,0 +1,21 @@
+
+# program 1 :-
+#
+
+# userInput = int(input("enter your num : "))
+# f = 1
+
+# for i in range(userInput , 0 ,  -1) :
+#     f*=i
+# print(f)
+
+
+
+# program 2 :-
+# pramid :-
+
+# s = "*"
+
+for i in range(1 ,  5 + 1 ) :
+    for a in range (1 , 5 +1):
+        print(a)
---
+++ python practise/day2/day2.py
@@ -0,0 +1,16 @@
+
+
+
+userInput= input("enter your number : ")
+inPalindrome = ("")
+
+inPalindrome = userInput[::-1]
+
+if(inPalindrome == userInput):
+    print("your given string is pslindrome")
+else:
+    print("your given string is not pslindrome")
+
+
+
+
