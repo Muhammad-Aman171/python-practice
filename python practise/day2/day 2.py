14871c5a47ab375d24755c970a2aefea1da0a8a1
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-14 10:47:59 +0500

    working

--- python practise/day2/day 2.py
+++ python practise/day2/day 2.py
@@ -1,8 +1,51 @@
 
 
-userInput = int(input("enter your number : "))
+# userInput = int(input("enter your number : "))
 
-num = 1
+# num = 1
 
-for i in range (1 , userInput +1)
-    print(i)
+# for i in range (1 , userInput +1):
+#     print(i)
+
+
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# number = int(input("Enter a number: "))
+# if is_prime(number):
+#     print("It's a prime number.")
+# else:
+#     print("It's not a prime number.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
