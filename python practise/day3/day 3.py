cb549c1818d42c23367d3a78dc8375fec817f279
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-15 19:05:24 +0500

    working

---
+++ python practise/day3/day 3.py
@@ -0,0 +1,28 @@
+
+
+
+# 5. Perfect Number Check:
+
+# userInput = int(input("enter your number : "))
+# total = 0
+# for i in range(1 , userInput ):
+#     if userInput % i == 0 :
+#         total +=  i
+#         print(i)
+# if total == userInput :
+#     print("this number is perfect number" , total)
+# else:
+#     print("this number is not perfect number" , total)
+
+
+
+# Sum of Digits Until Single Digit:
+
+# userInput = int(input("enter your number : "))
+
+# strNum = str(userInput)
+# total = 0
+# for i in strNum:
+#     total += int(i)
+# print(total)
+
