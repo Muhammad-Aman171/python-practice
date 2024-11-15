4596271923bd2fb505feb3262d23a86597873c70
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-15 23:50:17 +0500

    working

--- python practise/day3/day 3.py
+++ python practise/day3/day 3.py
@@ -44,18 +44,18 @@ for i in range(2 , userInput + 1):
 
 
 
-# User se ek number input lena
-num = int(input("Enter a number to find its prime factors: "))
+# # User se ek number input lena
+# num = int(input("Enter a number to find its prime factors: "))
 
-print("Prime factors are:", end=" ")
+# print("Prime factors are:", end=" ")
 
-# 2 se √num + 1 tak range define karte hain
-for factor in range(2, int(num**0.5) + 1):
-    # Jab tak num ko factor se divide kar sakte hain
-    while num % factor == 0:
-        print(factor, end=" ")
-        num = num // factor  # num ko factor se divide karte raho
+# # 2 se √num + 1 tak range define karte hain
+# for factor in range(2, int(num**0.5) + 1):
+#     # Jab tak num ko factor se divide kar sakte hain
+#     while num % factor == 0:
+#         print(factor, end=" ")
+#         num = num // factor  # num ko factor se divide karte raho
 
-# Agar num ab bhi 1 se bara hai, toh woh khud ek prime factor hai
-if num > 1:
-    print(num)
+# # Agar num ab bhi 1 se bara hai, toh woh khud ek prime factor hai
+# if num > 1:
+#     print(num)
