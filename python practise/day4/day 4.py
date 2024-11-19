a904c712b05faf6434bc44827936f97fdd299a2f
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-19 17:12:23 +0500

    programs

---
+++ python practise/day4/day 4.py
@@ -0,0 +1,47 @@
+
+
+
+# 5. Perfect Number Check:
+
+# userInput = int(input("enter your number : "))
+# total = 0
+# for i in range(1 , userInput ):
+#     if userInput % i == 0 :
+#         total +=  i
+#         print(i)
+# if total == userInput :
+#     print("this number is perfect number" , total)
+# else:
+#     print("this number is not perfect number" , total)
+
+
+
+# Sum of Digits Until Single Digit:
+
+# userInput = int(input("enter your number : "))
+# while userInput > 10:
+#     total = 0
+#     for i in str(userInput):
+#         total += int(i)
+#     userInput = total
+# print (userInput)
+
+
+# Prime Factors: 
+
+# userInput = int(input("enter your number : "))
+
+# primeFactorArr =  []
+
+# for i in range(2 , userInput + 1):
+#     while userInput % i == 0 :
+#         primeFactorArr.append(i)
+#         userInput = userInput // i
+#     if userInput == 1 : 
+#         break
+# print("prime factors of this number is ",primeFactorArr)
+
+
+
+
+
