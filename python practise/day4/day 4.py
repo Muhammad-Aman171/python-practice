119dd6563afa5394d67e45f8903633d2801b6bf4
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-20 22:38:54 +0500

    working

--- python practise/day4/day 4.py
+++ python practise/day4/day 4.py
@@ -2,19 +2,14 @@
 
 # Prime Factors: 
 
-userInput = int(input("enter your number : "))
+# userInput = int(input("enter your number : "))
 
-primeFactorArr =  []
+# primeFactorArr =  []
 
-for i in range(2 , userInput + 1):
-    while userInput % i == 0 :
-        primeFactorArr.append(i)
-        userInput = userInput // i
-    if userInput == 1 : 
-        break
-print("prime factors of this number is ",primeFactorArr)
-
-
-
-
-
+# for i in range(2 , userInput + 1):
+#     while userInput % i == 0 :
+#         primeFactorArr.append(i)
+#         userInput = userInput // i
+#     if userInput == 1 : 
+#         break
+# print("prime factors of this number is ",primeFactorArr)
